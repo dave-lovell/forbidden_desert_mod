2aab4769e87a488721c96ec23a8f9f677f8a6119
davelovellCARU <throwawaylovell@gmail.com>
2020-09-24 23:04:22 +0100

    secret_water_reserve added

--- assets/cards.R
+++ assets/cards.R
@@ -4,7 +4,16 @@ secret_water_reserve <- card(
   name = "Secret Water Reserve",
   description = "All players on your tile receive two water",
   effect <- function(player){
-    attr(player, "location") -> playerTile
+    attr(player, "location") -> cardPlayerTile
     
-  }
+    allPlayers <<- lapply(allPlayers,
+                             function(thisPlayer) {
+                               if(identical(
+                                 attr(thisPlayer, "location"),
+                                 cardPlayerTile)) {
+                                 thisPlayer <- give_water(thisPlayer, 2)
+                               }
+                               return(thisPlayer)
+                             })
+    }
 )
