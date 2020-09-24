d663f752ad76d99be3f0c5c32fe53af0ccaa10dd
davelovellCARU <throwawaylovell@gmail.com>
2020-09-24 22:53:35 +0100

    add_new_player function

--- assets/functions.R
+++ assets/functions.R
@@ -1,5 +1,27 @@
 ## Functions
 
+#### General Functions ####
+
+## Add new player -------
+
+add_new_player <- function(activePlayers = list()){
+  
+  allRoles <- list(
+    "Water Carrier" = player("Water Carrier"),
+    "Explorer" = player("Explorer"),
+    "Meteorologist" = player("Meteorologist"),
+    "Archaeologist" = player("Archaeologist"),
+    "Navigator" = player("Navigator"),
+    "Climber" = player("Climber")
+  )
+  
+  availableRoles <- allRoles[!(allRoles %in% activePlayers)]
+  
+  newActivePlayers <- c(activePlayers, sample(availableRoles, 1))
+  
+  return(newActivePlayers)
+}
+
 #### Player Functions ####
 
 give_water <- function(player = NULL,
